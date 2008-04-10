63d51539348a42dc115aedf3f7d9226bdfd40b09
Greg Price <price@mit.edu>
2008-04-10 02:36:01 +0000

    parallel-find.pl: work on Macs, and handle errors better
    * in find, use -print and a redirect rather than -fprint, which doesn't
      exist in the Mac's find
    * give a real error message when the directory exists, and die when we should
    
    Legacy-ID: 724

--- sbin/parallel-find.pl
+++ sbin/parallel-find.pl
@@ -7,8 +7,8 @@ use lib '/mit/scripts/sec-tools/perl';
 open(FILE, "</mit/scripts/sec-tools/store/scriptslist");
 my $dump = "/mit/scripts/sec-tools/store/versions";
 
-die if (-e $dump);
-`mkdir $dump`;
+(! -e $dump) || die "Output directory exists: $dump";
+system("mkdir", $dump) && die;
 
 use Proc::Queue size => 40, debug => 0;
 use POSIX ":sys_wait_h"; # imports WNOHANG
@@ -24,10 +24,11 @@ while (<FILE>) {
     my $f=fork;
     if(defined ($f) and $f==0) {
 	print "$user\n";
-#	print "find /mit/$user/web_scripts -name .scripts-version -fprint $dump/$user 2> /dev/null";
-	`find $homedir/web_scripts -xdev -name .scripts-version -fprint  $dump/$user 2> /dev/null`;
+	my $ret = system("find $homedir/web_scripts -xdev -name .scripts-version -print > $dump/$user 2> /dev/null");
 	sleep rand 1;
-	exit(0);
+	exit($ret);
     }
-    1 while waitpid(-1, WNOHANG)>0; # reaps childs
+    while (waitpid(-1, WNOHANG)>0) {
+        $? && die "child exited with error: $?";
+    }; # reaps childs
 }
