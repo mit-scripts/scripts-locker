67d315964751e056936fdabdd94c019d23d4f587
Greg Price <price@mit.edu>
2008-04-10 01:28:49 +0000

    remove obsolete get-passwd.sh, parse-passwd.pl
    Legacy-ID: 723

--- sbin/get-passwd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -f /mit/scripts/sec-tools/store/passwd
-scp -S /mit/scripts/bin/sshmic scripts.mit.edu:/etc/passwd /mit/scripts/sec-tools/store/passwd
-/mit/scripts/sec-tools/parse-passwd.pl /mit/scripts/sec-tools/store/passwd /mit/scripts/sec-tools/store/scriptslist
