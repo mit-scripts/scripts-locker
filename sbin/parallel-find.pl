f8e3059fdb43325272b311bae2cff90d0ad743ac
Greg Price <price@mit.edu>
2008-04-14 01:29:06 +0000

    more perl, less shell (for future enhancements)
    Temporarily I'm also ignoring stuff in e.g. the sipb cell, because for
    an undiagnosed reason those finds are hanging for me with my root tokens.
    
    Legacy-ID: 729

--- sbin/parallel-find.pl
+++ sbin/parallel-find.pl
@@ -10,7 +10,7 @@ my $dump = "/mit/scripts/sec-tools/store/versions";
 (! -e $dump) || die "Output directory exists: $dump";
 system("mkdir", $dump) && die;
 
-use Proc::Queue size => 40, debug => 0;
+use Proc::Queue size => 40, debug => 0, trace => 0;
 use POSIX ":sys_wait_h"; # imports WNOHANG
 
 # this loop creates new childs, but Proc::Queue makes it wait every
@@ -19,16 +19,30 @@ use POSIX ":sys_wait_h"; # imports WNOHANG
 # Note that we miss things where one volume is inside another if we
 # use -xdev.  May miss libraries stuff.
 
+sub find ($$) {
+    my $user = shift;
+    my $homedir = shift;
+
+    open my $files, "find $homedir/web_scripts -xdev -name .scripts-version 2>/dev/null |";
+    open my $out, ">$dump/$user";
+    while (my $f = <$files>) {
+        print $out $f;
+    }
+    return 0;
+}
+
 while (<FILE>) {
     my ($user, $homedir) = /^([^ ]*) (.*)$/;
     my $f=fork;
     if(defined ($f) and $f==0) {
+        if ($homedir !~ m|^/afs/athena|) {
+            print "ignoring non-athena-cell $user $homedir\n";
+            exit(0);
+        }
 	print "$user\n";
-	my $ret = system("find $homedir/web_scripts -xdev -name .scripts-version -print > $dump/$user 2> /dev/null");
+        $ret = find($user, $homedir);
 	sleep rand 1;
 	exit($ret);
     }
-    while (waitpid(-1, WNOHANG)>0) {
-        $? && die "child exited with error: $?";
-    }; # reaps childs
+    1 while waitpid(-1, WNOHANG)>0; # avoids memory leaks in Proc::Queue
 }
